32bca1049915592b7b43cacab806b9d59dfdbac7
Shikhar-S <shikhar.coder@gmail.com>
2017-09-19 13:23:09 +0530

    Cuda version of MergePath algorithm

--- merge_path.cu
+++ merge_path.cu
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -9,150 +8,177 @@
 
 /****************CODE STARTS HERE*******************/
 
-__device__ int satisfies(int i,int j,int *A,int *B)
+__device__ int satisfies(int i, int j, int *A, int *B)
 {
-	return (A[i]<=B[j]);
+	return (A[i] <= B[j]);
 }
 
-__global__ void MergePath(int *A,int *B,int* C,int *x,int *y,int n)
+__global__ void MergePath(int *A, int *B, int* C, int *x, int *y, int n)
 {
-    int num_of_threads=blockDim.x;
-    int idx=threadIdx.x;
-	if (idx==0)
+	int num_of_threads = blockDim.x;
+	int idx = threadIdx.x;
+	bool flag = false;
+	if (idx == 0)
 	{
-		x[idx]=0;
-		y[idx]=0;
-		return;
+		x[idx] = 0;
+		y[idx] = 0;
+		flag = true;
 	}
-	int A_start=idx*(2*n)/num_of_threads; //only when len(A)==len(B)
-	int B_start=max(0,A_start-(n-1));
-	A_start=min(n-1,A_start);
+	int A_start = idx*(2 * n) / num_of_threads; //only when len(A)==len(B)
+	int B_start = max(0, A_start - (n - 1));
+	A_start = min(n - 1, A_start);
 	int length_of_array;
-	
-	if(B_start==0)
-	{
-        
-	   length_of_array=A_start+1;
-    }
-	else
-		length_of_array=n-B_start;
 
-	int left=0,right=length_of_array-1;
-    // cout<<A_start<<" "<<B_start<<" "<<length_of_array<<endl<<"-------------------------------------------\n";
-	bool flag=false
-    while(left<=right && !flag)
+	if (B_start == 0)
 	{
-        // cout<<left<<" "<<right<<endl;
-		int mid=left+(right-left)/2;
-        int I=A_start-mid;
-        int J=B_start+mid;
-        if(!satisfies(I,J,A,B))
-        {
-            left=mid+1;
-        }
-        else
-        {
-            if(J==0)
-            {
-                x[idx]=(I+1);
-                y[idx]=(J);
-                flag=true;
-            }
-            else if(I==n-1)
-            {
-                x[idx]=(I+1);
-                y[idx]=(J);
-                flag=true;
-            }
-            else
-            {
-                if(!satisfies(I+1,J-1,A,B))
-                {
-                    x[idx]=(I+1);
-                    y[idx]=(J);
-                    flag=true;
-                }
-                else
-                {
-                    right=mid;
-                }
-            }
-        }
+
+		length_of_array = A_start + 1;
 	}
-    left--;
-    if(!flag)
-    {
-        x[idx]=(A_start-left);
-        y[idx]=(n);
-    }
-    __syncthreads();
+	else
+		length_of_array = n - B_start;
 
-    int end_x,end_y;
-    if(idx==num_of_threads-1)
-    {
-        end_x=n;
-        end_y=n;
-    }
-    else
-    {
-        end_x=x[idx+1];
-        end_y=y[idx+1];
-    }
-    int cur_x=x[idx];
-    int cur_y=y[idx];
-    int put_at=cur_x+cur_y;
-    while(cur_x<end_x && cur_y<end_y)
-    {
-        if(A[cur_x]<=B[cur_y])
-        {
-            C[put_at++]=A[cur_x++];
-        }
-        else
-        {
-            C[put_at++]=B[cur_y++];
-        }
-    }
-    while(cur_x<end_x)
-        C[put_at++]=A[cur_x++];
-    while(cur_y<end_y)
-        C[put_at++]=B[cur_y++];
+	int left = 0, right = length_of_array - 1;
+	// cout<<A_start<<" "<<B_start<<" "<<length_of_array<<endl<<"-------------------------------------------\n";
+	
+	while (left <= right && !flag)
+	{
+		// cout<<left<<" "<<right<<endl;
+		int mid = left + (right - left) / 2;
+		int I = A_start - mid;
+		int J = B_start + mid;
+		if (!satisfies(I, J, A, B))
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			if (J == 0)
+			{
+				x[idx] = (I + 1);
+				y[idx] = (J);
+				flag = true;
+			}
+			else if (I == n - 1)
+			{
+				x[idx] = (I + 1);
+				y[idx] = (J);
+				flag = true;
+			}
+			else
+			{
+				if (!satisfies(I + 1, J - 1, A, B))
+				{
+					x[idx] = (I + 1);
+					y[idx] = (J);
+					flag = true;
+				}
+				else
+				{
+					right = mid;
+				}
+			}
+		}
+	}
+	left--;
+	if (!flag)
+	{
+		x[idx] = (A_start - left);
+		y[idx] = (n);
+	}
+	__syncthreads();
+
+	int end_x, end_y;
+	if (idx == num_of_threads - 1)
+	{
+		end_x = n;
+		end_y = n;
+	}
+	else
+	{
+		end_x = x[idx + 1];
+		end_y = y[idx + 1];
+	}
+	int cur_x = x[idx];
+	int cur_y = y[idx];
+	int put_at = cur_x + cur_y;
+	while (cur_x<end_x && cur_y<end_y)
+	{
+		if (A[cur_x] <= B[cur_y])
+		{
+			C[put_at++] = A[cur_x++];
+		}
+		else
+		{
+			C[put_at++] = B[cur_y++];
+		}
+	}
+	while (cur_x<end_x)
+		C[put_at++] = A[cur_x++];
+	while (cur_y<end_y)
+		C[put_at++] = B[cur_y++];
 }
-void printArr(int *C)
+void printArr(int *C,int N)
 {
-    for(int i=0;i<C.size();i++)
-        cout<<C[i]<<" ";
-    cout<<endl;
+	FILE *g = fopen("g.txt", "w+");
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << C[i] << " ";
+		fprintf(g, "%d ", C[i]);
+	}
+	//std::cout << std::endl;
+
 }
 
 int main()
 {
-    int n;
-    cin>>n;
-    int *C=(int*)malloc(sizeof(int)*2*n);
-    int *A=(int*)malloc(sizeof(int)*n);
-    int *B=(int*)malloc(sizeof(int)*n);
-    int i=0;
-    for(int i=0;i<n;i++)
-    {
-        std::cin>>A[i];
-    }
-    for(int i=0;i<n;i++)
-    {
-    	std::cin>>B[i];
-    }
-    sort(A,A+n);
-    sort(B,B+n);
-    int num_of_threads;
-    std::cin>>num_of_threads;
-    int *x=(int*)malloc(sizeof(int)*num_of_threads);
-    int *y=(int*)malloc(sizeof(int)*num_of_threads);
-    HANDLE_ERROR(cudaMalloc(d_A,n*sizeof(int)));
-    HANDLE_ERROR(cudaMalloc(d_B,n*sizeof(int)));
-    HANDLE_ERROR(cudaMalloc(d_C,2*n*sizeof(int)));
-    HANDLE_ERROR(cudaMemcpy(d_A, A, N*sizeof(int), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(d_B,B,N*sizeof(int),cudaMemcpyHostToDevice));
-    MergePath<<<1,num_of_threads>>>(d_A,d_B,d_C,x,y,n);
-    HANDLE_ERROR(cudaMemcpy(C,d_C,2*N*sizeof(int),cudaMemcpyDeviceToHost));
-    printArr(C);
-    return 0;
+	int n;
+	FILE *f = fopen("f.txt", "r");
+	if (!f)
+	{
+		std::cout << "Error\n";
+		exit(-1);
+	}
+	fscanf(f,"%d", &n);
+	//std::cin >> n;
+	int *C = (int*)malloc(sizeof(int)* 2 * n);
+	int *A = (int*)malloc(sizeof(int)*n);
+	int *B = (int*)malloc(sizeof(int)*n);
+	
+	for (int i = 0; i<n; i++)
+	{
+		//std::cin >> A[i];
+		fscanf(f,"%d", A + i);
+	}
+	for (int i = 0; i<n; i++)
+	{
+		//std::cin >> B[i];
+		fscanf(f,"%d", B + i);
+	}
+	std::sort(A, A + n);
+	std::sort(B, B + n);
+
+	/*
+	for (int i = 0; i < n; i++)
+		std::cout << A[i] << " ";
+	std::cout << std::endl;
+	for (int i = 0; i < n; i++)
+		std::cout << B[i] << " ";
+	*/
+	int num_of_threads;
+	//std::cin >> num_of_threads;
+	fscanf(f, "%d", &num_of_threads);
+	
+	int *d_x,*d_y,*d_A,*d_B,*d_C;
+	HANDLE_ERROR(cudaMalloc((void**)&d_x,sizeof(int)*num_of_threads));
+	HANDLE_ERROR(cudaMalloc((void**)&d_y, sizeof(int)*num_of_threads));
+	HANDLE_ERROR(cudaMalloc((void**)&d_A, n*sizeof(int)));
+	HANDLE_ERROR(cudaMalloc((void**)&d_B, n*sizeof(int)));
+	HANDLE_ERROR(cudaMalloc((void**)&d_C, 2 * n*sizeof(int)));
+	HANDLE_ERROR(cudaDeviceSynchronize());
+	HANDLE_ERROR(cudaMemcpy(d_A, A, n*sizeof(int), cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(d_B, B, n*sizeof(int), cudaMemcpyHostToDevice));
+	MergePath << <1, num_of_threads >> >(d_A, d_B, d_C, d_x, d_y, n);
+	HANDLE_ERROR(cudaMemcpy(C, d_C, 2 * n*sizeof(int), cudaMemcpyDeviceToHost));
+	printArr(C,2*n);
+	return 0;
 }
