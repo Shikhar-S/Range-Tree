e8f9e6eff8e15435611afa56fcb8a444cdf076f4
Shikhar-S <shikhar.coder@gmail.com>
2017-09-19 13:50:35 +0530

    Matches from input and brute force result

--- check_merge.py
+++ check_merge.py
@@ -1,12 +1,25 @@
-#checks output of merge algorithm
-
 with open('g.txt','r') as G:
-	for line in G:
-		line=line.split()
-		line=[int(x) for x in line]
-		check_list=sorted(line)
-		for x,y in zip(line,check_list):
-			if(x != y):
-				print 'WRONG'
-				break
-		print 'CORRECT'
+    for line in G:
+        line=line.split()
+        line=[int(x) for x in line]
+        check_list=sorted(line)
+        with open('f.txt','r') as F:
+            n=0
+            num_ele=0
+            num_list=[]
+            num_list_=[]
+            for L in F:
+                if(n==0):
+                    num_ele=int(L)*2
+                elif(n==1):
+                    num_list=[int(x) for x in L.split()]
+                elif(n==2):
+                    num_list_=[int(x) for x in L.split()]
+                    for x in num_list_:
+                        num_list.append(x)
+                    num_list.sort()
+                n+=1
+            for x,y in zip(check_list,num_list):
+                if(x!=y):
+                    print('Wrong')
+print('correct')
