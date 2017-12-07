bc7d02e397c8d6d02d618e77aa697252ce6cb905
Shikhar S <shikhar.coder@gmail.com>
2017-12-07 14:07:00 +0530

    uploaded images

--- gen_test.py
+++ gen_test.py
@@ -1,14 +1,18 @@
 # -*- coding: utf8 -*-
+from __future__ import print_function
 import random
-n=input()
-n=2**int(n)
-lim=1000
-print(n)
-for i in range(n):
-	x=random.randint(-lim,lim)
-	y=random.randint(-lim,lim)
-	print(x,end=' ')
-	print(y,end='\n')
+f=open('f.txt','w+')
+for j in range(20):
+	n=j
+	n=2**int(n)
+	lim=1000
+	print(n,file=f)
+	for i in range(n):
+		x=random.randint(-lim,lim)
+		y=random.randint(-lim,lim)
+		print(x,end=' ',file=f)
+		print(y,end='\n',file=f)
+	print('Done with log-> ',j)
 '''
 queries=random.randint(1,10)
 print queries
---
+++ test.py
@@ -0,0 +1,23 @@
+import random
+n=32
+lim=1000
+print n
+for i in range(n):
+	x=random.randint(-lim,lim)
+	y=random.randint(-lim,lim)
+	print x, y
+
+queries=random.randint(1,10)
+print queries
+for _ in range(queries):
+	x_lo=random.randint(-lim,lim)
+	x_up=random.randint(-lim,lim)
+	y_lo=random.randint(-lim,lim)
+	y_up=random.randint(-lim,lim)
+	if(x_lo > x_up ):
+		x_lo, x_up = x_up,x_lo
+	if(y_lo > y_up):
+		y_lo, y_up = y_up, y_lo
+	print x_lo, y_lo
+	print x_up, y_up
+
